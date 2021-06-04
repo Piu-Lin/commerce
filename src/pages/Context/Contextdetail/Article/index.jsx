ba8a8573594f9f6ec28ab47ca2b9f7beb4b9db38
PiuLinx <15990799120@163.com>
2021-06-04 17:57:30 +0800

    完成面包屑导航以及作者添加正文项目
    正文项目中 fetch请求问题待解决

--- src/pages/Context/Contextdetail/Article/index.jsx
+++ src/pages/Context/Contextdetail/Article/index.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
-
+import './index.css'
 export default class Article extends Component {
+    state={
+        content:[],
+    }
+    getthat = async ()=>{
+        try{
+            const response = await fetch(`/public/word.json`)
+            console.log(response)
+            // const data = await response.json()
+            // console.log(data)
+           } catch (error){
+               console.log(error)
+           }
+    }
     render() {
+        this.getthat()
         return (
-            <div>
-                
+            <>
+            <div className="articletitlebox">
+                {/* error 无法复现下划线 */}
+                <div className="articlewordbox">学院简介</div>
             </div>
+            <div className="articleauthor">
+            作者：lrs :)  发布时间：2021-06-04  点击：666
+            </div>
+            <br />
+            <div className="articlecontent">
+                <div>
+                    
+                </div>
+            </div>
+            <br />
+            <div className="articlecontent">
+                <div>
+                阿里巴巴商学院以在互联网新经济领域的人才培养、研究、社会服务和创新创业教育为特色，特别是在信息经济、智能商业、电子商务服务、大数据分析、社交网络、网络营销等方向上优势明显。
+                </div>
+            </div>
+            </>
         )
     }
 }
--- src/pages/Context/Contextdetail/Crumb/index.jsx
+++ src/pages/Context/Contextdetail/Crumb/index.jsx
@@ -4,7 +4,16 @@ export default class Crumb extends Component {
     render() {
         return (
             <div className="crumbmainbox">
-                asdasd
+                <div className="crumbfloatleftbox">
+                    <div className="crumbtitle">当前位置：</div>
+                    <div className="crumbword">学院概况</div>
+                    <div className="luji"></div>
+                    <div className="crumbword">
+                        {/* 连接未添加 a标签样式去除未处理 */}
+                        学院简介
+                    </div>
+                </div>
+                
             </div>
         )
     }
