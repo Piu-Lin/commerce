bf39f01e02b256872e21765a20f885d7cd94c786
Piu-Lin <15990799120@163.com>
2021-06-06 18:17:53 +0800

    完成单一静态页面
    多数链接未制作

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ React 实现版本
 - 完成 Navheader 设计
 - 完成左侧导航设计
 - 完成面包屑设计 【未导航】
+- 完成主体页面设计
+- 完成底部栏设计
 
 ## 与目标版本的不同点
 
@@ -17,9 +19,7 @@ React 实现版本
 - 没有做搜索框出现效果
 - 去掉了搜索标志
 
-### 侧边导航部分
-
-- 路由连接未制作
+其余链接未制作，系标签样式未去除
 
 ## Available Scripts
 
@@ -27,23 +27,23 @@ In the project directory, you can run:
 
 ### `npm start`
 
-Runs the app in the development mode.\
+Runs the app in the development mode.
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
+The page will reload if you make edits.
 You will also see any lint errors in the console.
 
 ### `npm test`
 
-Launches the test runner in the interactive watch mode.\
+Launches the test runner in the interactive watch mode.
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm run build`
 
-Builds the app for production to the `build` folder.\
+Builds the app for production to the `build` folder.
 It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
+The build is minified and the filenames include the hashes.
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
--- src/pages/Footernav/index.jsx
+++ src/pages/Footernav/index.jsx
@@ -1,35 +1,53 @@
 import React, { Component } from 'react'
 import './index.css'
 export default class Footernav extends Component {
+    state={
+        comlink:[],
+    }
+    async componentDidMount(){
+        try{
+            const response = await fetch('./comlink.json')
+            const data = await response.json()
+            this.setState({comlink:data.comlink})
+           } catch (error){
+               console.log(error)
+           }
+    }
     render() {
+        const {comlink}=this.state
         return (
+            <>
             <div id="mainfootbox">
                 <div className="footnavtitlebox">
                     <div className="footnavtitle">常用连接</div>
                 </div>
-                <div className="footnavitembox">
-                    <div className="footnavitemcontr">
-                        <div className="footnavitem">
-                            <div className="pathlogo"></div>
-                            <div>教务处</div>
-                        </div>
-                        <div className="footnavitem">
-                            <div className="pathlogo"></div>
-                            <div>教务系统</div>
-                        </div>
-                        <div className="footnavitem">
-                            <div className="pathlogo"></div>
-                            <div>毕业论文</div>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="footnavitem2"> </div>
-                <div className="footnavitem"> </div>
-                <div className="footnavitem"> </div>
-                <div className="footnavitem"> </div>
-                <div className="footnavitem"> </div>
+                {
+                    comlink.map((footnavitems,boxkey)=>{
+                        return(
+                            <div className="footnavitembox" key={boxkey} >
+                                <div className="footnavitemcontr">
+                                    {
+                                        footnavitems.map((footnavitem,itemkey)=>{
+                                            return(
+                                                <div key={itemkey} className="footnavitem">
+                                                    <div className="pathlogo"/>
+                                                    <div className="footnavword">{footnavitem.name}</div>
+                                                    {/* footnavitem.url 为链接 */}
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            )
+                    })
+                }
             </div>
+            <div id="copyright">
+                <div className="copyrightword">版权所有 ©2008-2020 阿里巴巴商学院</div>
+                <div className="copyrightword">浙江省杭州市余杭区余杭塘路2318号（恕园1号楼）| 公安备案号：33011002011919 浙ICP备11056902号-1</div>
+            </div>
+            </>
         )
     }
 }
