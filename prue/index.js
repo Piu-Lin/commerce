9ad0ee7b33d0e7ec47c0c99e24bba8f2a44a5ff9
Piu-Lin <55540481+Piu-Lin@users.noreply.github.com>
2022-06-08 16:09:32 +0800

    update prue

---
+++ prue/index.js
@@ -0,0 +1,17 @@
+const leader=document.getElementById("leader")
+leader.addEventListener("click", ()=>{
+    window.location.href='leader.html';
+})
+
+const brief=document.getElementById("brief")
+brief.addEventListener("click", ()=>{
+    window.location.href='index.html';
+})
+const setting=document.getElementById("setting")
+setting.addEventListener("click", ()=>{
+    window.location.href='setting.html';
+})
+const council=document.getElementById("council")
+council.addEventListener("click", ()=>{
+    window.location.href='council.html';
+})
